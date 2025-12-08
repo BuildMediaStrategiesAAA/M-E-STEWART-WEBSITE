6625cf5db33d927b0872b773c057a13c3ba2926a
BuildMediaStrategies <buildmediastrategies@outlook.com>
2025-12-08 18:39:58 +0000

    Updated Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -65,11 +65,11 @@ export const Header: React.FC = () => {
         }}
       >
         <div className="flex items-center justify-between px-4 md:px-8 lg:px-16 py-0">
-          <Link to="/" className="flex items-center flex-shrink-0 -ml-2 md:-ml-4">
+          <Link to="/" className="flex items-center flex-shrink-0 -ml-2 md:-ml-4 py-1">
             <img
               src={logo}
               alt="M E Stewart Contractors"
-              className="h-36 md:h-44 w-auto object-contain"
+              className="h-20 md:h-28 w-auto object-contain leading-none"
             />
           </Link>
 
